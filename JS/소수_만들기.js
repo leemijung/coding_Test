36a9c1f59df83bdf64276b2a6b7b51656d9e9fe9
leemijung <76740863+leemijung@users.noreply.github.com>
2021-07-15 21:38:55 +0900

    Update 소수_만들기.js

--- JS/소수_만들기.js
+++ JS/소수_만들기.js
@@ -23,3 +23,19 @@ function isPrime(sum){
     }
     return true;
 }
+
+
+//풀이
+
+//nums 배열 내 3개의 숫자를 골라 더한 sum이 소수인 경우 카운드 (answer+=1)
+//sum 값으로 나올 수 있는 모든 경우를 배열에 push
+//for문을 이용해 sum 배열 값을 순서대로 접근
+//isPrime함수에 sum 인자를 하나씩 전달
+//isPrime 반환값이 true라면, 소수
+//answer 1 증가시킨다.
+
+//isPrime : 소수인가 아닌가
+//2부터 sum값 까지
+//i가 증가하면서 (i제곱=sum값)까지 확인
+//sum값을 i로 나눈 나머지가 0이라면 false
+//                         아니라면 true
