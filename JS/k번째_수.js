9c63fb90e71e968dff1604bf17ec8573b3cc4fdc
leemijung <76740863+leemijung@users.noreply.github.com>
2021-07-19 00:29:58 +0900

    Update k번째_수.js

--- JS/k번째_수.js
+++ JS/k번째_수.js
@@ -7,7 +7,7 @@ function solution(array, commands) {
         for(let i=commands[m][0]; i<=commands[m][1]; i++){
             tmp.push(array[i-1]);
         }
-        tmp.sort();
+        tmp.sort((a, b)=>a-b);
         for(let j=0; j<tmp.length; j++){
             if(j+1===commands[m][2]){
                 result.push(tmp[j]);
@@ -17,3 +17,14 @@ function solution(array, commands) {
 
     return result;
 }
+
+//<풀이>
+
+//sort( )
+//인자를 넣지 않으면
+//문자열로 오름차순 정렬하게 된다.
+//"10"이 "2"보다 앞 숫자로 인식된다.
+// => js에서 함수기능을 한다.
+
+//a-b가 0보다 작으면, b가 더 큰 수, b가 뒤에 위치
+//a-b가 0보다 크면,   a가 더 큰 수, a가 뒤에 
