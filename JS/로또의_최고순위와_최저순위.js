241b1fe3193589b5795909e3abe3c5e08e05ba38
leemijung <76740863+leemijung@users.noreply.github.com>
2021-07-20 20:54:58 +0900

    로또의_최고순위와_최저순위

---
+++ JS/로또의_최고순위와_최저순위.js
@@ -0,0 +1,47 @@
+function solution(lottos, win_nums) {
+    var answer = [0,0];
+    let zero_count=0;
+    let win_count=0;
+    
+    for(let i=0; i<lottos.length; i++){
+        if(lottos[i]===0){
+            zero_count++;
+            continue;
+        }
+        else{
+            for(let j=0; j<win_nums.length; j++){
+                if(lottos[i]===win_nums[j]){
+                    win_count++;
+                    win_nums.splice(j, 1);
+                }
+            }
+        }
+    }
+
+    answer[0]=win_count+zero_count;
+    answer[1]=win_count;        
+    
+    for(let i=0; i<2; i++){
+        if(answer[i]==6) answer[i]=1;
+        else if(answer[i]==5) answer[i]=2;
+        else if(answer[i]==4) answer[i]=3;
+        else if(answer[i]==3) answer[i]=4;
+        else if(answer[i]==2) answer[i]=5;
+        else answer[i]=6;
+    }
+    
+    return answer;
+}
+
+//<풀이>
+
+//zero_count : lottos 배열인자 중 0의 수
+//win_count : 맞춘 로또 번호 수
+
+//lottos 배열에서 0이면 zero_count 1증가
+//	           0이 아니면 win_nums의 인자들과 비교해서 같은지 확인한다.
+//만약 같다면 (로또 번호를 맞춘 경우)
+//win_count 1증가 시키고 win_nums 해당 인자를 삭제한다.
+
+//answer[0] : 가능한 최고 순위
+//answer[1] : 가능한 최저 순위
